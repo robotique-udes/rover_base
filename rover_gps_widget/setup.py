1cf6a8065fba1f63419a10bc0a83469e737ebedf
philippewarren <philippewarren31@gmail.com>
2020-10-25 14:33:30 -0400

    Initial creation of the gps_widget widget by copying and modifying cam_control

---
+++ rover_gps_widget/setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+d = generate_distutils_setup(
+    packages=['rover_gps_widget'],
+    package_dir={'': 'src'},
+    scripts=['scripts/rover_gps_widget'],
+)
+setup(**d)
